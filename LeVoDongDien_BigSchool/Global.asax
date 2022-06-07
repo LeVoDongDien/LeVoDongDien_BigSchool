70ddaab76679e3ebb92db7c7f9af1754254a7917
levod <levod@HUYENTRAM>
2022-06-08 00:04:18 +0700

    Add project files.

---
+++ LeVoDongDien_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeVoDongDien_BigSchool.MvcApplication" Language="C#" %>
---
+++ LeVoDongDien_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeVoDongDien_BigSchool.Startup))]
+namespace LeVoDongDien_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
